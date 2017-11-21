621f6bd3d42c93c3475b0d7a0f90883329d019d5
duhao <406507191@qq.com>
2017-11-21 18:02:51 +0800

    @dev create boom management system

---
+++ boom-management/src/components/AppAside.vue
@@ -0,0 +1,110 @@
+<template>
+	<!-- <el-aside> -->
+		<el-menu default-active="1-4-1" 
+		class="el-menu-vertical-demo hidden-sm-and-down s-aside-menu" 
+		@open="handleOpen" 
+		@close="handleClose" 
+		active-text-color="#42b983">
+		  <div class="log">VUEAmin</div>
+		  <el-submenu index="2">
+		    <template slot="title">
+		      <i class="el-icon-location"></i>
+		      <span slot="title">导航一</span>
+		    </template>
+		    <el-menu-item-group>
+		      <span slot="title">分组一</span>
+		      <el-menu-item index="2-1">选项1</el-menu-item>
+		      <el-menu-item index="2-2">选项2</el-menu-item>
+		    </el-menu-item-group>
+		    <el-menu-item-group title="分组2">
+		      <el-menu-item index="2-3">选项3</el-menu-item>
+		    </el-menu-item-group>
+		    <el-submenu index="2-4">
+		      <span slot="title">选项4</span>
+		      <el-menu-item index="2-4-1">选项1</el-menu-item>
+		    </el-submenu>
+		  </el-submenu>
+		  <el-menu-item index="3">
+		    <i class="el-icon-menu"></i>
+		    <span slot="title">导航二</span>
+		  </el-menu-item>
+		  <el-menu-item index="4">
+		    <i class="el-icon-setting"></i>
+		    <span slot="title">导航三</span>
+		  </el-menu-item>
+		  <el-menu-item index="5">
+		    <i class="el-icon-setting"></i>
+		    <span slot="title">导航四</span>
+		  </el-menu-item>
+		  <el-menu-item index="6">
+		    <i class="el-icon-setting"></i>
+		    <span slot="title">导航五</span>
+		  </el-menu-item>
+		  <el-menu-item index="7">
+		    <i class="el-icon-setting"></i>
+		    <span slot="title">导航六</span>
+		  </el-menu-item>
+		</el-menu>
+		<!-- <slot name="aside"></slot> -->
+	<!-- </el-aside> -->
+</template>
+<style>
+	a,a:visited {
+		text-decoration: none;
+		outline: none;
+	}
+  .el-menu-vertical-demo:not(.el-menu--collapse) {
+    /*width: 300px;*/
+    min-height: 400px;
+    z-index: 99999;
+  }
+  .s-aside-menu {
+  	height: 100%;
+  }
+  .log {
+	padding:15px;
+	font-weight:bolder;
+	font-size: 24px;
+	text-align:center;
+	color:#42b983;
+  }
+/*默认滚动条样式*/
+::-webkit-scrollbar {
+    width: 8px;
+    height: 8px;
+}
+::-webkit-scrollbar-track {
+    border: 1px #d3d3d3 solid;
+    box-shadow: 0px 0px 3px #dfdfdf inset;
+    border-radius: 10px;
+    background: #eee;
+}
+::-webkit-scrollbar-thumb {
+    border: 1px #808080 solid;
+    border-radius: 10px;
+    background: #999;
+}
+::-webkit-scrollbar-thumb:hover {
+    background: #7d7d7d;
+}
+</style>
+<script>
+  export default {
+    data() {
+      return {
+        isCollapse: false
+      };
+    },
+    methods: {
+      expand: function () {
+      	this.isCollapse = !this.isCollapse;
+      },
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
---
+++ boom-management/src/components/AppHeader.vue
@@ -0,0 +1,36 @@
+<template>
+    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal"  @select="handleSelect" active-text-color="#42b983">
+      <el-menu-item index="1">处理中心</el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">我的工作台</template>
+        <el-menu-item index="2-1">选项1</el-menu-item>
+        <el-menu-item index="2-2">选项2</el-menu-item>
+        <el-menu-item index="2-3" class="s-font">选项3</el-menu-item>
+      </el-submenu>
+      <el-menu-item index="3">
+        <a href="#" target="_blank">订单管理</a>
+      </el-menu-item>
+    </el-menu>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1'
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
+<style scoped lang="scss">
+  @import "../styles/base";
+
+  .s-font {
+    color: $primary-color;
+  }  
+</style>
