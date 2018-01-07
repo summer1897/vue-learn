58926a0d1d0c02c7666d3b5d432636d77f8846e2
summer1897 <406507191@qq.com>
2018-01-07 22:20:06 +0800

    @dev 添加日历组件

---
+++ vue-calendar/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <calendar/>
+  </div>
+</template>
+
+<script>
+import Calendar from './components/calendar'
+
+export default {
+  name: 'app',
+  components: {
+    Calendar
+  }
+}
+</script>
+
+<style>
+</style>
